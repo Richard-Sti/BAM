fab5d116b02eaed5b33540c067eafead417bb3d5
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-04-23 17:43:23 +0200

    add incremental importance scores

--- empiricalgalo/utils/__init__.py
+++ empiricalgalo/utils/__init__.py
@@ -14,3 +14,4 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from .scatter import (BinningAssignment, BivariateGaussianScatterPosterior)
+from .R2ordering import incremental_importance
